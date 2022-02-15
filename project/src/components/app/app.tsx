07b58e378a19a165987143d2e54295507de1cfab
ANDROMEDA-69 <andreyzhmurko4@gmail.com>
2022-02-15 16:22:04 +0200

    Добавляю компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,20 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import React from 'react';
+import Main from '../main/main-screen';
+
+type AppProps = {
+  promoFilmTitle: string;
+  promoFilmGenre: string;
+  promoFilmYear: number;
+};
+
+function App({
+  promoFilmTitle,
+  promoFilmGenre,
+  promoFilmYear,
+}: AppProps): JSX.Element {
+  return (
+    <Main promoFilmTitle={promoFilmTitle} promoFilmGenre={promoFilmGenre} promoFilmYear={promoFilmYear} />
+  );
 }
 
 export default App;
